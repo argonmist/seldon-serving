2171ad2b0b50c676cf3e9d3d17253b926f8eec03
Administrator <admin@example.com>
2020-02-13 17:24:44 +0800

    update rest api

--- rest-api/app.py
+++ rest-api/app.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import request, abort, Flask, jsonify
 import subprocess
 
@@ -15,6 +14,30 @@ def deploy():
     cmd = "helm install ./seldon-single-model --name " + dname + " --set model.env.MODEL_NAME=" + mname + " --namespace seldon-app"
     subprocess.call(cmd.split())
     return "Deploy Success" 
+
+@app.route('/model/delete', methods=['POST'])
+def delete():
+    if not request.json:
+        abort(400)
+
+    dname = request.json['deployname']
+    cmd = "helm del --purge " + dname
+    subprocess.call(cmd.split())
+    return "Delete Success"
+
+@app.route('/model/list', methods=['GET'])
+def list():
+    dpname = []
+    cmd = "helm list --namespace seldon-app | grep DEPLOYED"
+    result = str(subprocess.check_output(cmd.split()), encoding='utf-8')
+    result = result.splitlines()
+    for i in range(len(result)):
+        result[i] = result[i].split( )    
+        if i > 0:
+            dpname.append(result[i][0])
+    keys=['deployname']
+    d = dict.fromkeys(keys, dpname)
+    return jsonify(d)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
